da172f3d81368898ac3f41b8ca43a429396e42a4
leni <lenivanx@gmail.com>
2025-12-15 20:26:21 +0530

    updated header

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('babli-portfolio');
+  protected readonly linkedInLink = signal('https://www.linkedin.com/in/shreya-dey-89110b17b?utm_source=share_via&utm_content=profile&utm_medium=member_android');
+  protected readonly resumeLink = signal('');
+  protected readonly header_profile = signal('header_profile.jpg');
+  protected readonly header_arrow = signal('header_arrow.svg');
+  protected readonly header_box1 = signal('header_box1.png');
+  protected readonly header_box2 = signal('header_box2.png');
 }
